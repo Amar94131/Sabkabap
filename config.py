ee99a90de0abba169b86826662059cbba59af541
Amar94131 <jaysingh85029@gmail.com>
2025-02-22 22:23:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))  # Telegram API ID
+API_HASH = os.getenv("API_HASH")  # Telegram API Hash
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Bot Token
+CHANNEL_ID = os.getenv("CHANNEL_ID")  # Channel ID (e.g., @your_channel)
