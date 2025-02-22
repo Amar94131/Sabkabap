d88bdcd2bf3d4c5d712e14964f5a431c680a8400
Amar94131 <jaysingh85029@gmail.com>
2025-02-22 22:37:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
-API_ID = int(os.getenv("24389326"))  # Telegram API ID
-API_HASH = os.getenv("e516c423e3edd76854e1c0741863f6f0")  # Telegram API Hash
-BOT_TOKEN = os.getenv("7606038780:AAGiQs757sNQDp1rrJdPvOHqPyjnFn-S83o")  # Bot Token
-CHANNEL_ID = os.getenv("-1002156040011")  # Channel ID (e.g., @your_channel)
+#API_ID = int(os.getenv("24389326"))  # Telegram API ID
+#API_HASH = os.getenv("e516c423e3edd76854e1c0741863f6f0")  # Telegram API Hash
+#BOT_TOKEN = os.getenv("7606038780:AAGiQs757sNQDp1rrJdPvOHqPyjnFn-S83o")  # Bot Token
+#CHANNEL_ID = os.getenv("-1002156040011")  # Channel ID (e.g., @your_channel)
+
+
+API_ID = int(os.getenv("API_ID"))  # Correct way to fetch API_ID
+API_HASH = os.getenv("API_HASH")  
+BOT_TOKEN = os.getenv("BOT_TOKEN")  
+CHANNEL_ID = os.getenv("CHANNEL_ID")
