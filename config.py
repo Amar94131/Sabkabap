18a790a508d519e1215001ee11c5438610072626
Amar94131 <jaysingh85029@gmail.com>
2025-02-22 22:30:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))  # Telegram API ID
-API_HASH = os.getenv("API_HASH")  # Telegram API Hash
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # Bot Token
-CHANNEL_ID = os.getenv("CHANNEL_ID")  # Channel ID (e.g., @your_channel)
+API_ID = int(os.getenv("24389326"))  # Telegram API ID
+API_HASH = os.getenv("e516c423e3edd76854e1c0741863f6f0")  # Telegram API Hash
+BOT_TOKEN = os.getenv("7606038780:AAGiQs757sNQDp1rrJdPvOHqPyjnFn-S83o")  # Bot Token
+CHANNEL_ID = os.getenv("-1002156040011")  # Channel ID (e.g., @your_channel)
